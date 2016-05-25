ce39bd9902c49f9242fda9a5b643fe4c19c26631
Frederik Van Slycken <frederik.van.slycken@gmail.com>
2016-05-25 10:36:10 +0200

    Revert "experiment with zsh-autosuggestions"
    Tried it, didn't really like it. When I need a previous command, I'll
    just ctrl-R it.
    
    This reverts commit b9ba673af133e165d5caae31645aae8011e71622.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 ln -s ~/dotfiles/zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
--- zshrc
+++ zshrc
@@ -11,7 +11,7 @@ POWERLEVEL9K_STATUS_VERBOSE=false
 
 ENABLE_CORRECTION="true"
 
-plugins=(git vi-mode zsh-autosuggestions)
+plugins=(git vi-mode)
 
 # User configuration
 
