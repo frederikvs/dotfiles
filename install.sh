7b6a9d9210b1dd47e7bf38d2cf06416e04947974
Frederik Van Slycken <frederik.van.slycken@gmail.com>
2017-02-22 12:44:43 +0100

    install script : link zshrc after installation or it gets overwritten

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s ~/dotfiles/zshrc ~/.zshrc
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 ln -s ~/dotfiles/vimrc ~/.vimrc
