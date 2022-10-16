64c4ae8286d8b55bf7c94cd868f94178089d41c5
Frederik Van Slycken <frederik.van.slycken@gmail.com>
2022-10-16 15:20:51 +0200

    delete default .zshrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -f ~/.zshrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 
 ln -s ~/dotfiles/vimrc ~/.vimrc
